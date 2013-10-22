b5db5f6ba1e4bf7ea2ee73af5a3c969d4e24c489
Jeremy C. Andrus <jeremya@cs.columbia.edu>
2013-10-22 14:35:26 -0400

    Initial import of script
    Original script credit: Joe Grossberg.
    Updated for more modern Yahoo by: Jeremy C. Andrus
    
    Signed-off-by: Jeremy C. Andrus <jeremya@cs.columbia.edu>

---
+++ google_on_yahoo.js
@@ -0,0 +1,47 @@
+
+// Google Search on My Yahoo
+// version 0.2
+// 2005-05-26
+// Copyright (c) 2005, Joe Grossberg
+// under Creative Commons SA 1.0
+// http://creativecommons.org/licenses/sa/1.0/
+//
+// --------------------------------------------------------------------
+//
+// This is a Greasemonkey user script.
+//
+// To install, you need Greasemonkey: http://greasemonkey.mozdev.org/
+// Then restart Firefox and revisit this script.
+// Under Tools, there will be a new menu item to "Install User Script".
+// Accept the default configuration and install.
+//
+// To uninstall, go to Tools/Manage User Scripts,
+// select "Access Bar", and click Uninstall.
+//
+// --------------------------------------------------------------------
+//
+// ==UserScript==
+// @name          Google Search on My Yahoo
+// @version       0.2
+// @namespace     http://www.joegrossberg.com/googlemyyahoo.user.js
+// @description   use Google's search on the My Yahoo page
+// @match         https://my.yahoo.com*
+// @copyright     2005, Joe Grossberg, 2013, Jeremy C. Andrus
+// ==/UserScript==
+
+function fixSearchBox() {
+    var searchform = document.getElementById('UHSearch');
+    searchform.onsubmit = 'void(null)';
+    searchform.action = 'http://www.google.com/search?hl=en&btnG=Google+Search';
+    var searchinput = document.getElementById('UHSearchBox');
+    searchinput.name = 'q';
+    var subinput = document.getElementById('UHSearchProperty');
+    subinput.value = 'Google Search';
+    var subinputbtn = document.getElementById('UHSearchWeb');
+    subinputbtn.value = 'Google Search';
+}
+
+(function() {
+    document.addEventListener('load', fixSearchBox, false);
+    document.addEventListener('DOMSubtreeModified', fixSearchBox, false);
+})();
